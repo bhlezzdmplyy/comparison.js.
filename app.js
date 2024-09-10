94e4f19448e5c63731914d02c10ca7747c25c715
bhlezzdmplyy <felicitybhlezzgarbin@gmail.com>
2024-09-10 11:36:48 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+// Create two variables to store favorite numbers
+let firstFavNumb = 7;
+let secondFavNumb = 3;
+
+// Perform the comparisons and log the results
+
+// Greater than
+console.log("Greater than:", firstFavNumb > secondFavNumb);
+
+// Less than
+console.log("Less than:", firstFavNumb < secondFavNumb);
+
+// Greater than or equal to
+console.log("Greater than or equal to:", firstFavNumb >= secondFavNumb);
+
+// Less than or equal to
+console.log("Less than or equal to:", firstFavNumb <= secondFavNumb);
+
+// Strict equality (===)
+console.log("Strict equality (===):", firstFavNumb === secondFavNumb);
+
+// Loose equality (==)
+console.log("Loose equality (==):", firstFavNumb == secondFavNumb);
+
+// Strict inequality (!==)
+console.log("Strict inequality (!==):", firstFavNumb !== secondFavNumb);
+
+// Loose inequality (!=)
+console.log("Loose inequality (!=):", firstFavNumb != secondFavNumb);
+
+// AND
+console.log("AND:", firstFavNumb > secondFavNumb && firstFavNumb === 7);
+
+// OR
+console.log("OR:", firstFavNumb > secondFavNumb || secondFavNumb === 3);
+
+// NOT
+console.log("NOT:", !(firstFavNumb === secondFavNumb));
